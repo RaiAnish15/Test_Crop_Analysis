32025facd367f99b639ce916ae62ce75a1bddf0b
RaiAnish15 <114273678+RaiAnish15@users.noreply.github.com>
2025-02-11 16:41:13 +0530

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import numpy as np
 from arch import arch_model
 
 # Title of the dashboard
-st.title("Brinjal Price Analysis Across States and Districts")
+st.title("Analysis Across States and Districts")
 
 # Default image for the initial clean dashboard
 default_image = "India Map.jpeg"  # Replace with your actual default image file
